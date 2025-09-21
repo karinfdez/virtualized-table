2bbe92e952be11b160824e68d4db0e10cc7b4802
karinfdez <karinfdez@yahoo.com>
2025-09-20 23:34:07 -0400

    Update spinner and title

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center p-10">
-      <h1 className="text-2xl font-bold mb-6 text-indigo-600">
-        Product Table
+      <h1 className="text-2xl font-bold my-12 text-indigo-600">
+        A virtualized, infinite-scroll product table built with React, TanStack Table, and Virtuoso.
       </h1>
       <Table />
     </div>
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -2,30 +2,14 @@
 import { useReactTable, ColumnDef, flexRender, getCoreRowModel } from "@tanstack/react-table";
 import { useEffect, useState, CSSProperties } from "react";
 import { TableVirtuoso } from "react-virtuoso";
-
 import {Spinner} from './Spinner'
+
 type Product = {
   id: number;
   title: string;
   price: number;
   category: string;
 };
-
-const override: CSSProperties = {
-  display: "block",
-  margin: "0 auto",
-  borderColor: "red",
-};
-
-
-const mockData: Product[] = [
-    { id: 1, title: "iPhone 9", price: 549, category: "smartphones" },
-    { id: 2, title: "Samsung Galaxy S9", price: 499, category: "smartphones" },
-    { id: 3, title: "MacBook Pro", price: 1749, category: "laptops" },
-    { id: 4, title: "Dell XPS 13", price: 1299, category: "laptops" },
-		{ id: 5, title: "iPhone 13", price: 900, category: "smartphones" },
-		{ id: 6, title: "iPhone 16", price: 1200, category: "smartphones" },
-];
 
  // columns definition
     const columns: ColumnDef<Product>[] = [
@@ -56,28 +40,61 @@ export const Table = () => {
 
 	const [loading, setLoading] = useState(false)
 	const [error, setError] = useState('')
-	const [data, setData] = useState([])
+	const [data, setData] = useState<Product[]>([])
+	const [isFetchingMore, setIsFetchingMore] = useState(false);  //To control if data is being fetch
+	const [hasMore, setHasMore] = useState(true);   //To control is the API got to the final data
+	const [showNoMore, setShowNoMore] = useState(false);  //To show "no more data to load" message
 
 	const PAGE_LIMIT = 50
 
-		useEffect(() => {
+	const fetchElements = async () => {
+		try {
+			if (isFetchingMore || !hasMore) return;  //Dont execute another fetch if there is currently a request happening or there is no more data
+			setIsFetchingMore(true);  
+			const result = await fetch(`https://dummyjson.com/products?limit=${PAGE_LIMIT}&skip=${data.length}`) //using skip to always get the next data in the pagination, skiping the one used already
+			if(!result.ok) throw new Error('There was an error fetching products')
+			const finaData = await result.json()
+			if(finaData.products.length === 0) {
+				setHasMore(false); // no more data, avoid making requests continuously
+			}else{
+				// Simulating that data gets a few seconds to bet fetch
+				setTimeout(() => {
+					setData((prev) => [...prev, ...finaData.products]);
+					
+			}, 400)
+			}	
+		} catch(error:any) {
+			setError(error.message)
+		}finally{
+			setTimeout(() => {
+				setIsFetchingMore(false); 
+			}, 400)
+		}
+	}
+	
+// Initial load
+	useEffect(() => {
+		const fetchInitial = async () => {
+			setLoading(true);
+			const result = await fetch(`https://dummyjson.com/products?limit=${PAGE_LIMIT}&skip=0`);
+			const json = await result.json();
+			// To simulate that data takes time to be fetch
+			setTimeout(() => {
+					setData(json.products);
+					setLoading(false);
+			}, 500)
+		};
+		fetchInitial();
+	}, []);
 
-			const fetchElements = async () => {
-				try {
-					setLoading(true)
-					const result = await fetch(`https://dummyjson.com/products?limit=${PAGE_LIMIT}`)
-					if(!result.ok) throw new Error('There was an error fetching products')
-					const finaData = await result.json()
-					  setTimeout(() => {
-							setData(finaData.products);
-							setLoading(false); // ✅ only stop loading when data is set
-    				}, 800); //Added timeout to show spinner a little longer
-				} catch(error:any) {
-					setError(error.message)
-				}
-			}
-			fetchElements()
-		}, [])
+	useEffect(() => {
+		// If no more data, activate state for 3 seconds to show div
+  if (!hasMore) {
+    setShowNoMore(true);
+    const timer = setTimeout(() => setShowNoMore(false), 3000); // hide after 3s
+    return () => clearTimeout(timer);
+  }
+}, [hasMore]);
 
     const options = {
         data,
@@ -93,7 +110,7 @@ export const Table = () => {
 				<table className="w-full border-collapse text-sm text-left table-fixed">
 						{/* This gives virtualization. Allowing to load 1000 of data, but only showing certain amount in the DOM */}
 						<TableVirtuoso
-							style={{ height: "500px" }}
+							style={{ height: "700px" }}
 							data={data}
 							// Virtuoso attaches in props the style, children, ref
 							// These are the skeleton tags: table, thead, tbody, tr
@@ -106,8 +123,8 @@ export const Table = () => {
 								),
 								TableHead: (props) => <thead {...props} className="bg-indigo-100 text-indigo-700 text-center" />,
 								TableRow: (props) => <tr {...props} className="hover:bg-gray-100 transition-colors text-center" />,
-								TableBody: (props) => <tbody {...props} />,
-							}}
+								TableBody: (props) => <tbody {...props} />
+  						}}
 							// Tells Virtuoso what should render inside <thead>
 							fixedHeaderContent={() => (
 								<tr>
@@ -134,18 +151,32 @@ export const Table = () => {
 									</td>
 								));
 							}}
+							endReached={() => {
+    						fetchElements();
+  						}}
 						/>
 				</table>
+				{isFetchingMore && (
+					<div className="flex justify-center items-center py-4">
+						<div className="w-5 h-5 border-2 border-indigo-500 border-t-transparent rounded-full animate-spin"></div>
+						<span className="ml-2 text-sm text-indigo-600">Loading more...</span>
+					</div>
+				)}
+				{/* No more data is being fetch and there is no more data left in the API */}
+				{ !isFetchingMore && showNoMore && (
+					<div className="flex justify-center items-center py-4">
+						<span className="ml-2 text-sm text-indigo-600">No more data to load</span>
+					</div>
+				)}
 			</div>}
-			{loading && (
+			{loading && data.length === 0 && (
 				<Spinner
-					color="#4F46E5"   // Indigo
+					color="#4F46E5"  
 					loading={loading}
 				/>
 			)}
 			{error && <div className="text-red-500 font-medium">{error}</div>}
 		</>
-  
   )
 
 }
